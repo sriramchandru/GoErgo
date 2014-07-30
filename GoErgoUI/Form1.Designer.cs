5a79e4913f78d99e056e68e35811efd5b4b3f41b
sriramchandru <sriram.chandru@gmail.com>
2014-07-30 13:28:06 +0530

    GoErgo UI Added
    Addition of UI component

--- GoErgoUI/Form1.Designer.cs
+++ GoErgoUI/Form1.Designer.cs
@@ -29,11 +29,104 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureFace = new System.Windows.Forms.PictureBox();
+            this.pictureLeg = new System.Windows.Forms.PictureBox();
+            this.pictureBack = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureFace)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureLeg)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBack)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(9, 10);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open Port";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM11";
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(195, 509);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Inclination Angle";
+            // 
+            // pictureFace
+            // 
+            this.pictureFace.Image = global::GoErgoUI.Properties.Resources.face;
+            this.pictureFace.Location = new System.Drawing.Point(197, 46);
+            this.pictureFace.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureFace.Name = "pictureFace";
+            this.pictureFace.Size = new System.Drawing.Size(110, 94);
+            this.pictureFace.TabIndex = 4;
+            this.pictureFace.TabStop = false;
+            // 
+            // pictureLeg
+            // 
+            this.pictureLeg.Image = global::GoErgoUI.Properties.Resources.leg;
+            this.pictureLeg.Location = new System.Drawing.Point(197, 436);
+            this.pictureLeg.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureLeg.Name = "pictureLeg";
+            this.pictureLeg.Size = new System.Drawing.Size(232, 53);
+            this.pictureLeg.TabIndex = 3;
+            this.pictureLeg.TabStop = false;
+            // 
+            // pictureBack
+            // 
+            this.pictureBack.Image = global::GoErgoUI.Properties.Resources.back1;
+            this.pictureBack.Location = new System.Drawing.Point(97, 139);
+            this.pictureBack.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBack.Name = "pictureBack";
+            this.pictureBack.Size = new System.Drawing.Size(300, 292);
+            this.pictureBack.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBack.TabIndex = 2;
+            this.pictureBack.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(670, 573);
+            this.Controls.Add(this.pictureFace);
+            this.Controls.Add(this.pictureLeg);
+            this.Controls.Add(this.pictureBack);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureFace)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureLeg)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBack)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBack;
+        private System.Windows.Forms.PictureBox pictureLeg;
+        private System.Windows.Forms.PictureBox pictureFace;
     }
 }
 
--- GoErgoUI/Form1.cs
+++ GoErgoUI/Form1.cs
@@ -7,14 +7,95 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
+using System.Net.Sockets;
+using System.IO.Ports;
+using System.Drawing.Imaging;
+using System.Drawing.Drawing2D;
+
+//using goErgo;
+
 
 namespace GoErgoUI
 {
     public partial class Form1 : Form
     {
+        private string angle;
         public Form1()
         {
             InitializeComponent();
+            angle = "0";
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Thread eyeblink = new Thread()
+            serialPort1.Open();
+            string[] ports = SerialPort.GetPortNames();
+            //MessageBox.Show(ports.ToString());
+            
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+             
+            try
+            {
+                //MessageBox.Show(serialPort1.ReadLine());
+                this.label1.BeginInvoke((MethodInvoker) delegate() {
+                    double rotationAngleInt =0.0 ;
+                    try
+                    {
+                        string rotationAngle = serialPort1.ReadLine(); ;
+                        rotationAngleInt = Convert.ToDouble(rotationAngle);
+                    }
+                    catch(Exception ex)
+                    {
+                        MessageBox.Show("NaN in rotation angle. " + ex.Message);
+                    }
+                    RotateImg(pictureBack, "../../Resources/back.jpg", (rotationAngleInt-90.0).ToString());
+
+                }); 
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(String.Format("Came here, but something is wrong: " + ex.Message));
+            }
+        }
+
+
+        public static void RotateImg(PictureBox pb, String imgFile, string angle)
+        {
+            Image origimage = Image.FromFile(imgFile);
+            Image imgclone;
+            imgclone = origimage;
+            imgclone = ApplyTransparency((Bitmap)imgclone); //ADDING THIS
+            imgclone = (System.Drawing.Image)imgclone.Clone();
+            pb.Image = new Bitmap(rotateCenter(imgclone, angle));
+            pb.SizeMode = PictureBoxSizeMode.Normal;
+            //return 0;
+        }
+
+        public static Bitmap ApplyTransparency(Bitmap imagedata)
+        {
+            Bitmap bmp = imagedata;
+            bmp.MakeTransparent(Color.Fuchsia); // (0, 0, 0) is black
+
+            return bmp;
+        }
+
+        public static Bitmap rotateCenter(Image b, string angle)
+        {
+            Bitmap returnBitmap = new Bitmap(b.Width, b.Height + 1);
+            Graphics g = Graphics.FromImage(returnBitmap);
+            g.TranslateTransform((float)b.Width / 2, (float)b.Height / 2);
+            g.RotateTransform(-float.Parse(angle));
+            g.TranslateTransform(-(float)b.Width / 2, -(float)b.Height / 2);
+            g.DrawImage(b, b.Width / 2 - b.Height / 2, b.Height / 2 - b.Width / 2, b.Height, b.Width);
+
+            return returnBitmap;
+        }
+
+
     }
 }
