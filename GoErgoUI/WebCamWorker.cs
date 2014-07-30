3d26fb5f8662378760c2c930697231c313e4b5b7
sriramchandru <sriram.chandru@gmail.com>
2014-07-30 16:25:56 +0530

    Updating changes - Not working state

---
+++ GoErgoUI/WebCamWorker.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Management;
+
+namespace GoErgoUI
+{
+    /*
+[event_receiver(managed)]   // optional for native C++ and managed classes
+__gc class CReceiver {
+    public:
+   void BlinkHandler(int nValue) {
+   }
+   void hookEvent(CSource* pSource) {
+      __hook(&CSource::BlinkEvent, pSource, &CReceiver::BlinkHandler);
+   }
+   void unhookEvent(CSource* pSource) {
+      __unhook(&CSource::BlinkEvent, pSource, &CReceiver::BlinkHandler);
+   }
+}*/
+
+    unsafe public class WebCamWorker
+    {
+        [DllImport("GoErgo.dll")]
+        static extern int webCamMain();
+        [DllImport("GoErgo.dll")]
+        static extern int get_stats(int* blink, int* ambient_alarm, int* posture_alarm, int use_buf);
+        public void DoWork()
+        {
+            webCamMain();
+        }
+
+        public int[] GetStats()
+        {
+            while (true)
+            {
+
+            Thread.Sleep(1000);
+            int blink, ambient_alarm, posture_alarm;
+            get_stats(&blink, &ambient_alarm, &posture_alarm, 1);
+                int[] data = new int[3];
+                data[0] = blink;
+                data[1] = ambient_alarm;
+                data[2] = posture_alarm;
+            }
+        }
+    }
+}
